8f383ca7c7ab1909efc98a480d1d8d4319ff3fa5
Alok Menghrajani <alok@squareup.com>
2017-05-02 11:36:49 -0700

    Attempted fix...

--- src/test/java/alokmenghrajani/AppTest.java
+++ src/test/java/alokmenghrajani/AppTest.java
@@ -1,15 +1,10 @@
 package alokmenghrajani;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
-
-  @Rule public MockitoRule mockito = MockitoJUnit.rule();
 
   @Test
   public void myTest() {
